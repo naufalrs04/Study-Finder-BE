dc42aeedcd8a6a82be14cb3ebd2d0f3f3b78f9e8
naufalrs04 <naufalrizkisaputra2004@gmail.com>
2025-06-11 00:53:03 +0700

    fixing production

--- config/db.js
+++ config/db.js
@@ -3,13 +3,15 @@ import mysql from "mysql2/promise"; // Gunakan ES Module dari mysql2
 import dotenv from "dotenv";
 
 dotenv.config();
+// console.log("ENV DB_USER:", process.env.DB_USER);
+// console.log("ENV DB_PASSWORD:", process.env.DB_PASSWORD);
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
